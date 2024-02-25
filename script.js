02efa6240a87645656c95f749119a802dbfe675b
Jonathan Saunders <jonsaunders43@gmail.com>
2024-02-25 21:05:43 +0000

    Added grid generation function which creates divs and are styled using flexbox. Created divs will fit within the container and are of equal size.

--- script.js
+++ script.js
@@ -9,32 +9,51 @@ const colors = [
     "springgreen", "steelblue", "tomato", "turquoise", "violet", "wheat"
   ];
 
-function generateGrid(e){
-    let containerDiv = document.getElementById("containerDiv");
+const height = 500;
+const width = 500;
+let gridSize = 34;
+let gridDivSize = width/gridSize;
+let gridSpace = 100/gridSize;
+
+// Set the height and width of the container div
+let containerDiv = document.getElementById("containerDiv");
+containerDiv.style.height = `${height}px`;
+containerDiv.style.width = `${width}px`;
+
+// Test variable output
+let testoutput = document.getElementById("testoutput");
+testoutput.innerHTML = gridDivSize;
+let gridSpaceLabel = document.getElementById("gridSpaceLabel");
+gridSpaceLabel.innerHTML = gridSpace;
+
+function createGrid(){
     let gridDiv = document.createElement("div");
+    gridDiv.className = "gridDiv";
+    gridDiv.style.flex = "1 0" + `${gridSpace}%`;
+    //gridDiv.style.height = `${gridSpace}%`;
+    containerDiv.appendChild(gridDiv);
+
     var color = colors[Math.floor(Math.random()*colors.length)];
+
     gridDiv.addEventListener("mouseenter", (event) => {
         gridDiv.style.backgroundColor = color;
     })
-    gridDiv.className = "gridDiv";
-    gridDiv.style.border = "1px solid black";
-    gridDiv.style.height = "32px";
-    gridDiv.style.width = "32px";
-    containerDiv.appendChild(gridDiv);
 }
 
-let generate = 0;
-let squares = 0;
+for (let i = 0; i < ((gridSize*gridSize)); i++){
+    createGrid();
+}
+
+
 
 /*document.getElementById("size").onclick = function(){
     squares = prompt("How many squares?");
-    console.log(squares);
-}
+    gridDivSize = squares;
 
-console.log(squares);
-*/
-while (generate < 961){
-    generateGrid();
-    generate++;
+    createGrid();
 }
+*/
+
+
+
 
